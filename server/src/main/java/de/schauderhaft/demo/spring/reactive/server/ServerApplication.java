480b74a6b742ed08190697f370e01fa75314f4cd
Jens Schauder <jschauder@pivotal.io>
2017-11-03 09:33:47 +0100

    Simple Server setup

--- server/src/main/java/de/schauderhaft/demo/spring/reactive/server/ServerApplication.java
+++ server/src/main/java/de/schauderhaft/demo/spring/reactive/server/ServerApplication.java
@@ -1,12 +1,51 @@
 package de.schauderhaft.demo.spring.reactive.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+
+	@RestController
+	private static class PersonRestController {
+
+		@RequestMapping(value = "persons", method = RequestMethod.POST)
+		Mono<Person> addPerson(String name) {
+			return Mono.just(new Person(name));
+		}
+
+		@RequestMapping(value = "persons", method = RequestMethod.GET)
+		Flux<Person> persons() {
+			return Flux.just(new Person("Oliver"), new Person("Mark"), new Person("Christoph"));
+		}
+	}
+
+	@Document
+	@Data
+	private static class Person {
+
+		@Id final String id;
+
+		String name;
+
+		List<String> olderPersonsOfSameName = new ArrayList<>();
 	}
 }
